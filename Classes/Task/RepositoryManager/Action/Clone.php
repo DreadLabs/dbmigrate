d89d8fb9d0e27c198ac53b2e3206664023b29158
Thomas Juhnke <tommy@van-tomas.de>
2013-03-23 13:32:01 +0100

    [TASK] adding clone action for TYPO3 slave instances

---
+++ Classes/Task/RepositoryManager/Action/Clone.php
@@ -0,0 +1,12 @@
+<?php
+require_once t3lib_extMgm::extPath('dbmigrate', 'Classes/Task/RepositoryManager/AbstractAction.php');
+
+class Tx_Dbmigrate_Task_RepositoryManager_Action_Clone extends Tx_Dbmigrate_Task_RepositoryManager_AbstractAction {
+
+	public function getOptions() {
+	}
+
+	public function process() {
+	}
+}
+?>
