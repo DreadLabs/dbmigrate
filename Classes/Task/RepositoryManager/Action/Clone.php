17b3a6dde75cb9a480f2aba43d9e0d826fa0f39c
Thomas Juhnke <tommy@van-tomas.de>
2013-03-23 13:42:43 +0100

    [TASK] finishing clone task action

--- Classes/Task/RepositoryManager/Action/Clone.php
+++ Classes/Task/RepositoryManager/Action/Clone.php
@@ -3,10 +3,40 @@ require_once t3lib_extMgm::extPath('dbmigrate', 'Classes/Task/RepositoryManager/
 
 class Tx_Dbmigrate_Task_RepositoryManager_Action_Clone extends Tx_Dbmigrate_Task_RepositoryManager_AbstractAction {
 
+	protected static $command = 'git clone %repository% %targetPath% 2>&1';
+
+	protected static $targetPath = 'Resources/Public/Migrations/';
+
 	public function getOptions() {
+		$this->options[] = array(
+			'label' => $this->getTranslation('task.action.clone.field.repository.label'),
+			'field' => '<input name="repository" value="" size="60" />',
+		);
 	}
 
 	public function process() {
+		try {
+			$content = $this->cloneRepository();
+		} catch (Exception $e) {
+			$content .= $e->getMessage();
+		}
+
+		return $content;
+	}
+
+	protected function cloneRepository() {
+		if (TRUE === file_exists(t3lib_extMgm::extPath('dbmigrate', self::$targetPath . '.git'))) {
+			throw new Exception('The repository is already cloned!');
+		}
+
+		$replacePairs = array(
+			'%repository%' => escapeshellcmd(t3lib_div::_GP('repository')),
+			'%targetPath%' => escapeshellcmd(t3lib_extMgm::extPath('dbmigrate', self::$targetPath)),
+		);
+
+		$command = strtr(self::$command, $replacePairs);
+
+		$this->executeCommand($command, 'Clonging the repository failed. Please see the following output for further details:');
 	}
 }
 ?>
